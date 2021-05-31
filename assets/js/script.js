c2c1a678cb272f0f937ae52ac932463b4a1a9428
algmoreno <alg.moreno00@gmail.com>
2021-05-30 22:19:42 -0700

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,16 +6,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event){
-    console.log("activate", this); 
+    $(this).addClass("dropover"); 
+    $(".bottom-trash").addClass("bottom-trash-drag"); 
   },
   deactivate: function(event) {
-    console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag"); 
   },
   over: function(event) {
-    console.log("over", event.target);
+    $(event.target).addClass("dropover-active");
   },
   out: function(event) {
-    console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
   },
   update: function(event) {
     var tempArr= []; 
@@ -54,9 +56,11 @@ $("#trash").droppable({
   drop: function(event, ui) {
     console.log("drop");
     ui.draggable.remove();
+    $(".bottom-trash").removeClass(".bottom-trash-active");
   },
   over: function(event, ui) {
     console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
     console.log("out");
@@ -130,7 +134,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -260,5 +264,11 @@ $("#remove-tasks").on("click", function() {
   saveTasks();
 });
 
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el){
+    auditTask(console.log(taskEl));
+    
+  })
+},1800000); 
 // load tasks for the first time
 loadTasks();
